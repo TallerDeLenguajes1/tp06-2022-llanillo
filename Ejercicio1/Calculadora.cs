0f62e7326ce1de1b71626fac3f4e396f30105000
Octavio Llanillo <sirshanisho@gmail.com>
2022-05-25 11:03:29 -0300

    TP 6 Finalizado

---
+++ Ejercicio1/Calculadora.cs
@@ -0,0 +1,46 @@
+﻿namespace Ejercicio1;
+
+public class Calculadora
+{
+
+    private double _resultado;
+
+    public Calculadora(double inicial)
+    {
+        _resultado = inicial;
+    }
+
+    public double Sumar(double termino)
+    {
+        _resultado += termino;
+        return _resultado;
+    }
+
+    public double Restar(double termino)
+    {
+        _resultado -= termino;
+        return _resultado;
+    }
+
+    public double Multiplicar(double termino)
+    {
+        _resultado *= termino;
+        return _resultado;
+    }
+
+    public double Dividir(double termino)
+    {
+        _resultado /= termino;
+        return _resultado;
+    }
+
+    public void Limpiar()
+    {
+        _resultado = 0;
+    }
+
+    public double VerResultado()
+    {
+        return _resultado;
+    }
+}
---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Ejercicio1;
+
+static class Program
+{
+
+    public static int Main(string[] args)
+    {
+        int bucle = 0;
+        int opcion;
+        double entrada;
+        Calculadora calculadora = new Calculadora(0);
+
+        while(bucle == 0)
+        {
+            Console.WriteLine("1. Sumar");
+            Console.WriteLine("2. Restar");
+            Console.WriteLine("3. Multiplicar");
+            Console.WriteLine("4. Dividir");
+            Console.WriteLine("5. Salir");
+            
+            Console.WriteLine("Resultado: " + calculadora.VerResultado());
+            
+            opcion = Convert.ToInt32(Console.ReadLine());
+            
+            if(opcion != 5)
+            {
+                Console.WriteLine("Ingrese el numero");
+                entrada = float.Parse(Console.ReadLine());
+
+                switch(opcion)
+                {
+                    case 1:
+                        calculadora.Sumar(entrada);
+                        break;
+                    case 2:
+                        calculadora.Restar(entrada);
+                        break;
+                    case 3:
+                        calculadora.Multiplicar(entrada);
+                        break;
+                    case 4:
+                        calculadora.Dividir(entrada);
+                        break;
+                }
+            }
+            else
+            {
+                bucle = 1;
+            }
+        }
+
+        return 0;
+    }
+}
